cda74c290e65cdc8d98f8524284487eee71782d7
Marissa <marissa.shieh@gmail.com>
2019-04-25 16:19:44 -0700

    Create framework for upcoming connections

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-// const findStock = require('../database/Stock.js');
-// const findTag = require('../database/Tag.js');
 const compression = require('compression');
-const request = require('./request.js');
+const request = require('./indexPG.js');
 
 const app = express();
 const port = 3003;
@@ -13,15 +11,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(compression());
 
-app.listen(port, () => console.log('APP IS LISTENING'));
+app.listen(port, () => console.log(`APP IS LISTENING ON ${port}`));
 
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
-app.use('/stocks/:symbol', express.static(path.join(__dirname, '../client/dist')));
+app.use('/stocks/*', express.static(path.join(__dirname, '../client/dist')));
 
+// COMPANY INFO ENDPOINTS
 app.get('/api/about/:symbol', (req, res) => {
   request.findStock(req.params.symbol).then(data => res.send(data));
 });
 
+app.post('/api/about/:symbol', (req, res) => {
+  request.findStock(req.params.symbol).then(data => res.send(data));
+});
+
+app.put('/api/about/:symbol', (req, res) => {
+  request.findStock(req.params.symbol).then(data => res.send(data));
+});
+
+app.delete('/api/about/:symbol', (req, res) => {
+  request.findStock(req.params.symbol).then(data => res.send(data));
+});
+
+
+// USER INFO ENDPOINTS
 app.get('/api/user/:userId', (req, res) => {
+  request.findUser(req.params.userId).then(data => res.send(data));
+});
+
+app.post('/api/user/:userId', (req, res) => {
+  request.findUser(req.params.userId).then(data => res.send(data));
+});
+
+app.put('/api/user/:userId', (req, res) => {
+  request.findUser(req.params.userId).then(data => res.send(data));
+});
+
+app.delete('/api/user/:userId', (req, res) => {
   request.findUser(req.params.userId).then(data => res.send(data));
 });
